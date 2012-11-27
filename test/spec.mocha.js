26ddce69ec7c513e50cf9b2c244f5a505efc0689
Jeff Barczewski <jeff.barczewski@gmail.com>
2012-11-27 14:10:58 -0600

    add tests that error is emitted

--- test/spec.mocha.js
+++ test/spec.mocha.js
@@ -4,6 +4,10 @@
 var spec = require('stream-spec');
 var tester = require('stream-tester');
 var passStream = require('..'); // require('pass-stream');
+var Stream = require('stream');
+var chai = require('chai-stack');
+
+var t = chai.assert;
 
 suite('stream-spec');
 
@@ -50,3 +54,58 @@ test('spec random stream with transform fns', function (done) {
     .on('error', function (err) { done(err); })
     .on('end', function () { done(); });
 });
+
+// erroring source stream
+
+test('on source error, stream should emit error', function (done) {
+  var ps = passStream();
+  spec(ps)
+    .through({
+      strict: false,
+      error: true
+    })
+    .validateOnExit();
+
+  var source = new Stream();
+  source
+    .pipe(ps)
+    .on('error', function (err) {
+      t.equal(err.message, 'my error');
+      done();
+    });
+
+  process.nextTick(function () {
+    source.emit('error', new Error('my error'));
+  });
+});
+
+test('on source error w/transferFns, stream should emit error', function (done) {
+  function writeFn(data) {
+    /*jshint validthis:true */
+    this.queueWrite(data);
+  }
+  function endFn() {
+    /*jshint validthis:true */
+    this.queueEnd();
+  }
+  var ps = passStream(writeFn, endFn);
+  spec(ps)
+    .through({
+      strict: false,
+      error: true
+    })
+    .validateOnExit();
+
+  var source = new Stream();
+  source
+    .pipe(ps)
+    .on('error', function (err) {
+      t.equal(err.message, 'my error');
+      done();
+    });
+
+  process.nextTick(function () {
+    source.emit('error', new Error('my error'));
+  });
+});
+
